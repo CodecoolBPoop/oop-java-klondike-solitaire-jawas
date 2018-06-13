5f9b523af3f52dc92f7edb81fbf2e0dc33e99857
Papp Gergely <pappgergo096@gmail.com>
2018-06-13 14:36:36 +0200

    restart

--- src/com/codecool/klondike/Game.java
+++ src/com/codecool/klondike/Game.java
@@ -2,8 +2,10 @@ package com.codecool.klondike;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ListChangeListener;
+import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Background;
@@ -12,6 +14,7 @@ import javafx.scene.layout.BackgroundPosition;
 import javafx.scene.layout.BackgroundRepeat;
 import javafx.scene.layout.BackgroundSize;
 import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -97,6 +100,7 @@ public class Game extends Pane {
         deck = Card.createNewDeck();
         initPiles();
         dealCards();
+        posButton();
     }
 
     public void addMouseEventHandlers(Card card) {
@@ -205,5 +209,32 @@ public class Game extends Pane {
                 BackgroundRepeat.REPEAT, BackgroundRepeat.REPEAT,
                 BackgroundPosition.CENTER, BackgroundSize.DEFAULT)));
     }
+
+    private EventHandler<ActionEvent> restart = e -> {
+        getChildren().clear();
+        stockPile.clear();
+        foundationPiles.clear();
+        tableauPiles.clear();
+        discardPile.clear();
+        draggedCards.clear();
+        deck = Card.createNewDeck();
+        initPiles();
+        dealCards();
+        posButton();
+
+//        Klondike reStart = new Klondike();
+//        Stage primary = new Stage();
+//        reStart.start(primary);
+    };
+
+
+    public void posButton() {
+        Button btn = new Button("Restart");
+        btn.setLayoutY(5);
+        btn.setLayoutX(5);
+        btn.setOnAction(restart);
+        getChildren().add(btn);
+    }
+
 
 }
